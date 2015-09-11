d96f31e1ca7614c72ced455956c5ca5b3d3fb936
uyab <uyab.exe@gmail.com>
2015-09-11 17:05:13 +0700

    remove blade section

--- resources/views/semantic-ui/buttons.blade.php
+++ resources/views/semantic-ui/buttons.blade.php
@@ -16,54 +16,51 @@
 </div>
 
 
-@section('script-end')
-    @parent
-    <script type="text/javascript">
-        $(document).ready(function () {
-            $('.buttons-votee').on('click', '.button', function () {
-                var btn = $(this);
-                btn.addClass('loading');
-                btn.siblings().andSelf().addClass('disabled').attr('disabled', true);
+<script type="text/javascript">
+    $(document).ready(function () {
+        $('.buttons-votee').on('click', '.button', function () {
+            var btn = $(this);
+            btn.addClass('loading');
+            btn.siblings().andSelf().addClass('disabled').attr('disabled', true);
 
-                $.ajax({
-                    url: btn.data('url'),
-                    type: "POST",
-                    dataType: 'json',
-                    data: {id: btn.data('id')}
-                })
-                        .done(function (response, status, xhr) {
-                            if (xhr.status == 200) {
-                                btn.parent().find('.total-up').html(response.data.vote_up);
-                                btn.parent().find('.total-down').html(response.data.vote_down);
-                                btn.parent().find('.button-vote').removeClass('green red');
+            $.ajax({
+                url: btn.data('url'),
+                type: "POST",
+                dataType: 'json',
+                data: {id: btn.data('id')}
+            })
+                    .done(function (response, status, xhr) {
+                        if (xhr.status == 200) {
+                            btn.parent().find('.total-up').html(response.data.vote_up);
+                            btn.parent().find('.total-down').html(response.data.vote_down);
+                            btn.parent().find('.button-vote').removeClass('green red');
 
-                                switch (response.data.value) {
-                                    case {{ config('votee.values.up') }}:
-                                        btn.parent().find('.button-vote-up').addClass('green');
-                                        break;
-                                    case {{ config('votee.values.down') }}:
-                                        btn.parent().find('.button-vote-down').addClass('red');
-                                        break;
-                                    case {{ config('votee.values.neutral') }}:
-                                        break;
-                                }
-                            } else {
-                                btn.popup({html: xhr.responseJSON.message}).popup('show');
+                            switch (response.data.value) {
+                                case {{ config('votee.values.up') }}:
+                                    btn.parent().find('.button-vote-up').addClass('green');
+                                    break;
+                                case {{ config('votee.values.down') }}:
+                                    btn.parent().find('.button-vote-down').addClass('red');
+                                    break;
+                                case {{ config('votee.values.neutral') }}:
+                                    break;
                             }
-                        })
-                        .fail(function (xhr) {
-                            btn.popup({content: xhr.responseJSON.message}).popup('show');
-                        })
-                        .always(function () {
-                            btn.siblings().andSelf().removeClass('disabled').removeAttr('disabled');
-                            btn.removeClass('loading');
-                        });
-            });
+                        } else {
+                            btn.popup({html: xhr.responseJSON.message}).popup('show');
+                        }
+                    })
+                    .fail(function (xhr) {
+                        btn.popup({content: xhr.responseJSON.message}).popup('show');
+                    })
+                    .always(function () {
+                        btn.siblings().andSelf().removeClass('disabled').removeAttr('disabled');
+                        btn.removeClass('loading');
+                    });
+        });
 
-            $('.button-vote').popup({
-                on: 'manual'
-            });
+        $('.button-vote').popup({
+            on: 'manual'
+        });
 
-        })
-    </script>
-@endsection
+    })
+</script>
