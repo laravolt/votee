2b1afd1441b2fa3c718514f873b63f0626f43b1c
uyab <uyab.exe@gmail.com>
2015-10-23 14:57:20 +0700

    view styling

--- resources/views/semantic-ui/buttons.blade.php
+++ resources/views/semantic-ui/buttons.blade.php
@@ -1,17 +1,17 @@
-<div class="ui buttons buttons-votee {{ $options['class'] }}">
+<div class="ui buttons buttons-votee {{ array_get($options, 'size') }}">
     <button id="button-like"
-       data-url="{{ route('votee.up') }}"
-       data-id="{{ $content['id'] }}"
-       data-type="{{ get_class($content) }}"
-       class="ui button button-vote button-vote-up {{ ($vote && $vote['is_up']) ? 'green' : '' }}">
+            data-url="{{ route('votee.up') }}"
+            data-id="{{ $content['id'] }}"
+            data-type="{{ get_class($content) }}"
+            class="ui button button-vote button-vote-up grey {{ array_get($options, 'class') }} {{ ($vote && $vote['is_up']) ? 'green' : '' }}">
         <i class="icon thumbs outline up"></i>
         <span class="total-up"> {{ $content['vote_up'] }}</span>
     </button>
     <button id="button-dislike"
-       data-url="{{ route('votee.down') }}"
-       data-id="{{ $content['id'] }}"
-        data-type="{{ get_class($content) }}"
-       class="ui button button-vote button-vote-down {{ ($vote && $vote['is_down']) ? 'red' : '' }}">
+            data-url="{{ route('votee.down') }}"
+            data-id="{{ $content['id'] }}"
+            data-type="{{ get_class($content) }}"
+            class="ui button button-vote button-vote-down grey {{ array_get($options, 'class') }} {{ ($vote && $vote['is_down']) ? 'red' : '' }}">
         <i class="icon thumbs outline down"></i>
         <span class="total-down"> {{ $content['vote_down'] }}</span>
     </button>
